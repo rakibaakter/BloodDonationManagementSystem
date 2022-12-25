66b398c8386f57ed3cb242584e10530227cb46f6
RakibaAKter <64217963+rakibaakter@users.noreply.github.com>
2022-12-25 17:03:28 +0600

    day 2 working on login page

---
+++ src/blooddonationmanagementsystemfinal/DBConnection.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blooddonationmanagementsystemfinal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author mahbub
+ */
+public class DBConnection {
+    Connection cons = null;
+    
+    public static Connection ConnectionDB(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection con = DriverManager.getConnection("sdbc:sqlite:UsersDB.db");
+            System.out.println("Connection Successfully");
+            return con;
+        }catch(Exception e){
+            System.out.println("Connection failed due to "+e);
+            return null;
+        }
+    }
+}
